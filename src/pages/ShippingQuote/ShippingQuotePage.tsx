72fe2e0a3fb174438c5e3f23ca5782b4bc478b72
oscar <devoscarsilva@gmail.com>
2025-03-13 11:57:03 -0300

    Agregue el mapa en la seccion cotizar envios

--- src/pages/ShippingQuote/ShippingQuotePage.tsx
+++ src/pages/ShippingQuote/ShippingQuotePage.tsx
@@ -1,5 +1,22 @@
-export function ShippingQuotePage (){
-    return <div>
-        <h1>ShippingQuotePage</h1>
-    </div>
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+
+export function ShippingQuotePage() {
+    return (
+        <div style={{height: "100vh", width: "100%", padding: 0, margin: 0}}>
+            <MapContainer
+                center={[-27.3621, -55.8913]}
+                zoom={15}
+                style={{ height: "100vh", width: "100%", zIndex: 0 }}
+            >
+                <TileLayer
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    attribution='&copy; OpenStreetMap contributors'
+                />
+                <Marker position={[51.505, -0.09]}>
+                    <Popup>Ubicación de prueba</Popup>
+                </Marker>
+            </MapContainer>
+        </div>
+    );
 }
